a3db927d4d54bd250aeb0c0ae993bdcd1770aa38
isabellaCaspari <ica@adorsys.de>
2020-12-01 11:51:15 +0100

    Add Part 1 and Part 2

--- 01_puzzel.kt
+++ 01_puzzel.kt
@@ -1,7 +1,5 @@
 fun main() {
-    val coins = intArrayOf(1587,
-        1407,
-        1717,
+    val coins = intArrayOf(1587, 1407,1717,
         1596,
         1566,
         1752,
@@ -200,7 +198,18 @@ fun main() {
         1809,
         1576 )
 
-    for (i in coins.indices) {
+
+        println("Puzzel 1, Part 1")
+        for (i in coins.indices) {
+            for (j in i + 1 until coins.size) {
+                    if (coins[i] + coins[j]  == 2020) {
+                        println(coins[i] * coins[j] )
+                    }
+                }
+            }
+    
+        println("Puzzel 1, Part 2")
+        for (i in coins.indices) {
         for (j in i + 1 until coins.size) {
             for (x in j + 1 until coins.size) {
                 if (coins[i] + coins[j] + coins[x] == 2020) {
