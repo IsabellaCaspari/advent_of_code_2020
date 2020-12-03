eec7abd23c4751d1702a351de098abcbbd463580
isabellaCaspari <ica@adorsys.de>
2020-12-03 11:30:00 +0100

    puzzle 3

--- 03_puzzel.kt
+++ 03_puzzel.kt
@@ -1,40 +1,78 @@
 import java.io.File
 
-val passwords = mutableListOf<PasswordSpecification>()
 const val GREEN_BOLD = "\u001b[1;32m"
 const val BLACK = "\u001b[0;30m"
 
 fun main() {
-    readInputFile("02_puzzel_input.txt")
     partOne()
     partTwo()
 }
 
 private fun partOne() {
-    println(GREEN_BOLD + "Puzzel Day 3, Part 1 ")
+    println("""${GREEN_BOLD}Puzzel Day 3, Part 1 """)
+
+    //Right 3, down 1.
+    val result = findWaySlope(3, 1)
+
+    println("""${BLACK}Thats the answer $result""")
+
 }
 
 private fun partTwo() {
     println(GREEN_BOLD + "Puzzel Day 3, Part 2 ")
-    println(BLACK + "There are " + numberOfValidPasswords + " passwords valid of " + passwords.size)
+
+    /*Right 1, down 1.
+      Right 3, down 1.
+      Right 5, down 1.
+      Right 7, down 1.
+      Right 1, down 2.*/
+
+    val result: Long = findWaySlope(1, 1).toLong() *
+            findWaySlope(3, 1).toLong() *
+            findWaySlope(5, 1).toLong() *
+            findWaySlope(7, 1).toLong() *
+            findWaySlope(1, 2).toLong()
+
+    println("""${BLACK}Thats the answer $result""")
 }
 
-private fun readInputFile(filename: String) {
+
+private fun findWaySlope(right: Int, down: Int): Int {
+    var map = readInputFile("03_puzzel_input.txt")
+    var lines = 0
+    var columns = 0
+    var numbersOfTree = 0
+
+    while (lines <= map.size - 1) {
+        if (map[lines][columns] == '#') {
+            map[lines][columns] = '0'
+            numbersOfTree++
+        } else {
+            map[lines][columns] = 'X'
+        }
+        lines = lines + down
+        columns = columns + right
+    }
+    return numbersOfTree
+}
+
+
+private fun readInputFile(filename: String): MutableList<MutableList<Char>> {
+    var map: MutableList<MutableList<Char>> = ArrayList()
     File(filename).forEachLine {
         try {
-            val spec = PasswordSpecification()
-            // Separate string in diffrent parts
-            // String input: 9-10 n: nnnnnngnzxnnn
-            val parts = it.split("-", " ", ": ")
-            spec.min = parts[0].toInt()
-            spec.max = parts[1].toInt()
-            spec.letter = parts[2].single()
-            spec.password = parts[3]
-            passwords.add(spec)
-
+            // squares (.) and trees (#)
+            //..##.........##.........##.........##.........##.........##.......
+            // multiply pattern X times
+            var linepattern = ""
+            for (i in 0..100) {
+                linepattern += it
+            }
+            map.add(linepattern.toCharArray().toMutableList())
         } catch (e: java.lang.Exception) {
-            println("Thats a Cast exception, sth went wrong while casting the string")
+            println(e)
         }
     }
+    return map
 }
 
